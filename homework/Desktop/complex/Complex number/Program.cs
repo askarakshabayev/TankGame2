bc76bf57b1a04287f6bb30a0bf245804763d692f
aidact <aida.ct127@gmail.com>
2017-02-09 12:47:26 +0600

    added program

---
+++ homework/Desktop/complex/Complex number/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Program
+    {
+        class Complex //create a class 
+        {
+            //added data
+            public int x;
+            public int y;
+            public Complex(int x, int y) //make a constructor
+            {
+                //initialize class attributes
+                this.x = x;
+                this.y = y;
+            }
+            //rewrite function ToString()
+            public override string ToString()
+            {
+                return x + "/" + y;
+            }
+            //function to find gcd of two complex numbers
+            static int gcd(int x, int y)
+            {
+                if (x == 0)
+                    return y;
+                return gcd(y % x, x);
+            }
+            //provide overload a binary operator +
+            public static Complex operator +(Complex x, Complex y)
+            {
+                //changed the purpose of  +  by adding new class 
+                Complex c = new Complex((x.x * y.y + y.x * x.y)/ gcd(x.x * y.y + y.x * x.y, x.y * y.y), (x.y * y.y) / (gcd(x.x * y.y + y.x * x.y, x.y * y.y)));
+
+
+
+                return (c);
+                
+           
+            }
+        
+            //provide overload a binary operator -
+            public static Complex operator -(Complex x, Complex y)
+            {
+                //changed the purpose of  -  by adding new class 
+                Complex c = new Complex((x.x * y.y - y.x * x.y)/ gcd(x.x * y.y + y.x * x.y, x.y * y.y), x.y * y.y/ gcd(x.x * y.y + y.x * x.y, x.y * y.y));
+                return c;
+            }
+            //provide overload a binary operator /
+            public static Complex operator /(Complex x, Complex y)
+            {
+                //changed the purpose of  /  by adding new class 
+                Complex c = new Complex(x.x * y.y/gcd(x.x * y.y, x.y * y.x), x.y * y.x/ gcd(x.x * y.y, x.y * y.x));
+                return c;
+            }
+            //provide overload a binary operator *
+            public static Complex operator *(Complex x, Complex y)
+            {
+                //changed the purpose of  *  by adding new class 
+                Complex c = new Complex(x.x * y.x/ gcd(x.x * y.x, x.y * y.y), x.y * y.y / gcd(x.x * y.x, x.y * y.y));
+
+                return c;
+            }
+            static void Main(string[] args) // function to split complex numbers
+            {
+                string s = Console.ReadLine();
+                string[] arr = s.Split(); // breaks a complex numbers into parts until space: 1/2 3/4
+                Complex sum = new Complex(0, 0); // created class and declared atributes as zeros
+                foreach (string ss in arr) // instead of for we use foreach to work with string array
+                {
+                    string[] t = ss.Split('/'); // breaks a complex numbers into parts until "/": 1 2 3  4
+                    Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
+                    if (sum.x == 0 & sum.y == 0) // because we have declared class as zeros, first condition is always true
+                    {
+                        sum = p;
+                    }
+                    else
+                    {
+                        sum = sum + p; // adds one complex number to another 
+                    }
+                }
+
+                Complex sub = new Complex(0, 0); // created class and declared atributes as zeros
+                foreach (string ss in arr)
+                {
+                    string[] t = ss.Split('/');// breaks a complex numbers into parts until "/": 1 2 3  4
+                    Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
+                    if (sub.x == 0 & sub.y == 0) // because we have declared class as zeros, first condition is always true
+                    {
+                        sub = p; // we declare first element as an first complex number that we write through console
+                                 
+                    }
+                    else
+                    {
+                        sub = sub - p;
+                    }
+                }
+                Complex mul = new Complex(0, 0); // created class and declared atributes as zeros
+                foreach (string ss in arr) // 
+                {
+                    string[] t = ss.Split('/');
+                    Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
+                    if (mul.x == 0 & mul.y == 0) // because we have declared class as zeros, first condition is always true
+                    {
+                        mul = p;
+                         // we declare first element as an first complex number that we write through console
+                         // if we wrote like that  mul.x = p.x;  mul.x = p.y;, it would be easier to understand what happens
+
+                    }
+                    else
+                    {
+                        mul = mul * p; // multiply current complex number with previous
+                    }
+                }
+
+                Complex div = new Complex(0, 0);
+                foreach (string ss in arr) // instead of for we use foreach to work with string array
+                {
+                    string[] t = ss.Split('/');
+                    Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
+                    if (div.x == 0 & div.y == 0)
+                    {
+                        div = p; // we declare first element as an first complex number that we write through console
+                    }
+                    else
+                    {
+                        div = div / p; // divides current complex number by previous
+                    }
+
+                }
+                // coz we have changed Tostring(), now it shows atributs of complex number as an complex number
+                Console.WriteLine(sum); 
+                Console.WriteLine(sub); 
+                Console.WriteLine(div); 
+                Console.WriteLine(mul); 
+
+
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
---
+++ homework/Desktop/complex/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace complex
+{
+    class Complex
+    {
+        public int x, y;
+        public int a, nod = 1, d = 1;
+        public int z, q;
+        public Complex(int x, int y) // создаем конструктор
+        {
+        this.x = x; // инициализируем атрибуты класса
+        this.y = y;
+        }
+    
+
+           public static Complex operator +(Complex c1, Complex c2) // осуществляем перегрузку бинарного оператора +
+        {
+            Complex c = new Complex(c1.x*c2.y+c2.x*c1.y, c1.y*c2.y); // меняем работу оператора + с помощью нового класса
+            return c;
+        }
+         /*   public int gcd(int a, int b)
+           {
+               if (b == 0)
+                   return a;
+
+               return gcd(b, a % b);
+           }*/
+       
+        static void Main(string[] args)
+           {
+               int a, nod = 1, d = 1, z, q;
+               
+               
+               string inp = Console.ReadLine(); // прочитываем строку
+               string[] arr = inp.Split(); // отделяем строку от пробелов
+
+               Complex sum = new Complex(0, 1);
+               foreach (string s in arr)
+               {
+                   
+                   string[] t = s.Split('/'); 
+                   Complex p = new Complex(int.Parse(t[0]), int.Parse(t[1]));
+                   sum = sum + p;
+               }
+               z = sum.x; // заводим новую численную переменную, в которой сохраняем атрибут класса
+               q = sum.y; // заводим новую численную переменную, в которой сохраняем атрибут класса
+               a = z;
+               if (q > z) a = q; // алгоритм нахождения НОД
+               for (int i = 1; i <= a; i++)
+               {
+                   if (d > nod) nod = d;
+                   if (z % i == 0 && q % i == 0) 
+                   {
+                       d = i;
+                   }
+               }
+                Console.WriteLine(z/nod+"/"+q/nod); // разделяем атрибуты класса на НОД для того, чтобы дробь сокращалась
+
+               
+                
+               
+                 Console.ReadKey();
+
+           }
+    }
+}
