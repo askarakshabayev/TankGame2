9d3995a7afe39487c3b8c61749d11528d90961b4
aidact <aida.ct127@gmail.com>
2017-04-04 18:01:08 +0600

    Danara_Aida

---
+++ TankGame2/WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        Tank tank = new Tank();
+      
+     
+        
+        public Form1()
+        {
+            InitializeComponent();
+      
+        }
+        
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           tank.Up(e.Graphics);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tank.direction == Tank.Direction.DOWN)
+            {
+                tank.y += 10;
+                tank.y1 += 10;
+                tank.y2 += 10;
+               
+            }
+            if (tank.direction == Tank.Direction.RIGHT)
+            {
+                tank.x += 10;
+                tank.x1 += 10;
+                tank.x2 += 10;
+                
+            }
+            if (tank.direction == Tank.Direction.LEFT)
+            {
+                tank.x -= 10;
+                tank.x1 -= 10;
+                tank.x2 -= 10;
+
+            }
+            if (tank.direction == Tank.Direction.UP)
+            {
+                tank.y -= 10;
+                tank.y1 -= 10;
+                tank.y2 -= 10;
+
+            }
+            Refresh();
+        }
+        public void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.S)
+            {
+                tank.direction = Tank.Direction.DOWN;
+                tank.x2 = tank.x1;
+                tank.y2 = tank.y1 + 65;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                tank.direction = Tank.Direction.RIGHT;
+                tank.x2 = tank.x1 + 65;
+                tank.y2 = tank.y1;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                tank.direction = Tank.Direction.LEFT;
+                tank.x2 = tank.x1 - 65;
+                tank.y2 = tank.y1;
+            }
+            if (e.KeyCode == Keys.W)
+            {
+                tank.direction = Tank.Direction.UP;
+                tank.x2 = tank.x1;
+                tank.y2 = tank.y1 - 65;
+            }
+        }
+       
+
+       
+    }
+}
---
+++ TankGame2/WindowsFormsApplication7/Tank.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace WindowsFormsApplication7
+{
+    class Tank
+    {
+        public int x = 10, y = 10, len = 100, w = 100, h = 100, r;
+        public int x1 = 60, y1 = 60, x2 = 125, y2 = 60;
+        public enum Direction
+        {
+            UP,
+            DOWN,
+            LEFT,
+            RIGHT
+        }
+        public Direction direction = Direction.RIGHT;
+        
+
+        public void Up(Graphics g)
+        {
+            g.DrawEllipse(new Pen(Color.Red), x, y, w, h);
+            g.DrawLine(new Pen(Color.Black), x1, y1, x2, y2);
+
+        }
+        
+
+        public void Left(Graphics g)
+        {
+
+        }
+
+        public void Right(Graphics g)
+        {
+
+        }
+    }
+}
